8f20e320d7bebefeac8fe1d0bf6ea708bf676f8b
cjwgray74 <blackfusion@gmail.com>
2025-02-23 23:04:52 -0600

    clear up the CRUD for a better understanding.

--- employee-service/src/main/java/com/ibm/fscc/employeeservice/dto/ResponseDto.java
+++ employee-service/src/main/java/com/ibm/fscc/employeeservice/dto/ResponseDto.java
@@ -3,10 +3,6 @@ package com.ibm.fscc.employeeservice.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-/*The `ResponseDto` class in Spring Boot is a data transfer object (DTO) that represents a response from the server to the client.
-It is used to transfer data between the client and the server,
-and it provides a simple and lightweight way to represent the response data.*/
-
 @Data //Used to generate getter and setter methods automatically.
 @AllArgsConstructor //Used to generate an all-arguments constructor automatically.
 public class ResponseDto {

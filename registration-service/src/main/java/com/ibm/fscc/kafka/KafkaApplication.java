d8cb1c1e3ac8eed42dc48a46c7d81b2efb5d0c44
cjwgray74 <blackfusion@gmail.com>
2025-02-27 10:46:00 -0600

    Setup packages for Kafka.

--- registration-service/src/main/java/com/ibm/fscc/kafka/KafkaApplication.java
+++ registration-service/src/main/java/com/ibm/fscc/kafka/KafkaApplication.java
@@ -1,40 +1,17 @@
 package com.ibm.fscc.kafka;
 
-import com.ibm.fscc.kafka.repository.KafkaRepository;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootApplication
-@EnableKafka
-public class KafkaApplication implements CommandLineRunner {
+@EnableMongoRepositories
+public class KafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
 
-	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//	}
-//	@Autowired
-//	private KafkaRepository kafkaRepository;
-//
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//	}
 }

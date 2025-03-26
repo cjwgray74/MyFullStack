0d66436ed6f779ea58409ab0d2f8f2ae7641d02b
cjwgray74 <blackfusion@gmail.com>
2025-03-25 21:59:20 -0500

    Updated the login dockerfile and image

--- login_db_docker_image/schema.sql
+++ login_db_docker_image/schema.sql
@@ -1,8 +1,22 @@
-CREATE TABLE `login` (
-                         `id` bigint NOT NULL AUTO_INCREMENT,
-                         `email` varchar(255) DEFAULT NULL,
-                         `password` varchar(255) NOT NULL,
-                         `username` varchar(255) NOT NULL,
-                         PRIMARY KEY (`id`),
-                         UNIQUE KEY `UK3cs7dxhnine2f6x5ic4741s6e` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE `user` (
+                        `email` varchar(50) NOT NULL,
+                        `password` char(68) NOT NULL,
+                        PRIMARY KEY (`email`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE authorities (
+                             id INT AUTO_INCREMENT,
+                             username VARCHAR(50) NOT NULL,
+                             authority VARCHAR(50) NOT NULL,
+                             PRIMARY KEY (id),
+                             CONSTRAINT fk_authorities_username FOREIGN KEY (username) REFERENCES user (email) ON DELETE CASCADE
+
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE UNIQUE INDEX uk_users_username ON user (email);
+
+CREATE INDEX idx_authorities_username ON authorities (username);
+
+CREATE INDEX idx_authorities_authority ON authorities (authority);
